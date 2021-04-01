1ef5f675408df6a3bc3e88f530c8bfa1795dac71
Thuc Vu <thuc.vu@aegona.com>
2021-04-01 13:30:29 +0700

    init

---
+++ src/add.ts
@@ -0,0 +1,4 @@
+export const add = (a = 1, b = 2) => a + b
+export const khongxai = () => {
+  console.log('Dòng này sẽ không có trong file build')
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { add } from '@@/src/add'
+import './loadImage'
+import './index.scss'
+
+console.log(`add${add(1, 2)}`)
